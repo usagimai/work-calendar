3f61858e45012a3e64ef575232bc5b6d53ba9a64
Kotake <82296998+usagimai@users.noreply.github.com>
2022-02-15 11:56:19 +0800

    adjust to show dynamic data from firestore

--- src/components/reusable/EditWorkCLD.js
+++ src/components/reusable/EditWorkCLD.js
@@ -8,44 +8,53 @@ import { SmallButtonDark } from "./ButtonCollection";
 import AlertMessage from "./AlertMessage";
 import { formData } from "../../data";
 
-const EditWork = ({
+const EditWorkCLD = ({
   status,
-  setStatus,
   allowScroll,
-  projectData,
-  setPjEditWorkOpen,
-  work,
-  setTdEditWorkOpen,
-  todo,
+  setEventPopover,
+  eventSelected,
+  projects,
+  todos,
 }) => {
   const [title, setTitle] = useState();
+  const [projectData, setProjectData] = useState();
+  const [workData, setWorkData] = useState();
 
   const handleEditWorkClose = () => {
-    switch (status.page) {
-      case "PJ":
-        setPjEditWorkOpen(false);
-        setStatus({ ...status, action: "view-project" });
-        allowScroll();
+    setEventPopover(false);
+    allowScroll();
+  };
+
+  useEffect(() => {
+    if (!projects || !todos) return;
+
+    switch (eventSelected.resource[0]) {
+      case "projects":
+        const selectedProject = projects.find((project) => {
+          return project.id === eventSelected.resource[1];
+        });
+        setProjectData(selectedProject);
+
+        const selectedWork = selectedProject.works.find((work) => {
+          return eventSelected.title.match(new RegExp(work.content, "gi"));
+        });
+        setWorkData(selectedWork);
         break;
-      case "TD":
-        setTdEditWorkOpen(false);
-        allowScroll();
+      case "todos":
+        const selectedTodo = todos.find((todo) => {
+          return todo.id === eventSelected.resource[1];
+        });
+        setWorkData(selectedTodo);
         break;
       default:
         break;
     }
-  };
+  }, [projects]);
 
   useEffect(() => {
     switch (status.action) {
-      case "create-pj-work":
-        setTitle(["新增工作細項", ""]);
-        break;
       case "edit-pj-work":
         setTitle(["編輯工作細項", "工作細項完成 / 刪除"]);
-        break;
-      case "create-td-work":
-        setTitle(["新增待辦事項", ""]);
         break;
       case "edit-td-work":
         setTitle(["編輯待辦事項", "待辦事項完成 / 刪除"]);
@@ -61,7 +70,7 @@ const EditWork = ({
         <div className="close-bg" onClick={handleEditWorkClose}>
           <IconSelector name="close" />
         </div>
-        {title && (
+        {title && workData && (
           <div className="create-content">
             <div>
               <DecorationTitle title={title[0]} fontSize="m" />
@@ -73,32 +82,20 @@ const EditWork = ({
             </div>
             <div className="edit-work-info">
               <div className="m-text">完成期限</div>
-              {status.action === "create-pj-work" ||
-              status.action === "create-td-work" ? (
-                <SelectCalendar text="選擇" />
-              ) : (
-                <div>
-                  <div className="m-text">
-                    {work ? work.deadline : todo.deadline}
-                  </div>
-                  <SelectCalendar text="編輯" />
-                </div>
-              )}
+              <div>
+                <div className="m-text">{workData.deadline}</div>
+                <SelectCalendar text="編輯" />
+              </div>
 
               <div className="m-text">執行日期</div>
-              {status.action === "create-pj-work" ||
-              status.action === "create-td-work" ? (
-                <SelectCalendar text="選擇" />
-              ) : (
-                <SelectCalendar text="檢視/編輯" />
-              )}
+              <SelectCalendar text="檢視/編輯" />
 
               <div className="m-text">執行內容</div>
               <input
                 type="text"
                 maxLength="30"
                 className="s-text"
-                value={work ? work.content : todo ? todo.content : ""}
+                value={workData.content}
               />
               <div></div>
               <div className="xs-text gray-color limit">{formData.limit30}</div>
@@ -112,7 +109,7 @@ const EditWork = ({
               <textarea
                 maxLength="100"
                 className="s-text"
-                value={work ? work.remark : todo ? todo.remark : ""}
+                value={workData.remark ? workData.remark : ""}
               ></textarea>
               <div></div>
               <div className="xs-text gray-color limit">
@@ -120,31 +117,23 @@ const EditWork = ({
               </div>
             </div>
 
-            {status.action === "create-pj-work" ||
-            status.action === "create-td-work" ? null : (
-              <>
-                <DecorationTitle title={title[1]} fontSize="m" />
-                <div className="edit-work-final">
-                  <div className="m-text">實際完成日</div>
-                  {(work && work.finishDate === "未完成") ||
-                  (todo && todo.finishDate === "未完成") ? (
-                    <SelectCalendar text="選擇" />
-                  ) : (
-                    <div>
-                      <div className="m-text">
-                        {work ? work.finishDate : todo.finishDate}
-                      </div>
-                      <SelectCalendar text="編輯" />
-                    </div>
-                  )}
-
-                  <div className="m-text">刪除工作細項</div>
-                  <div className="pointer">
-                    <IconSelector name="delete" />
-                  </div>
+            <DecorationTitle title={title[1]} fontSize="m" />
+            <div className="edit-work-final">
+              <div className="m-text">實際完成日</div>
+              {workData.finishDate === "未完成" ? (
+                <SelectCalendar text="選擇" />
+              ) : (
+                <div>
+                  <div className="m-text">{workData.finishDate}</div>
+                  <SelectCalendar text="編輯" />
                 </div>
-              </>
-            )}
+              )}
+
+              <div className="m-text">刪除工作細項</div>
+              <div className="pointer">
+                <IconSelector name="delete" />
+              </div>
+            </div>
           </div>
         )}
 
@@ -157,4 +146,4 @@ const EditWork = ({
   );
 };
 
-export default EditWork;
+export default EditWorkCLD;
