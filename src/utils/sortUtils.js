f1a57528970214a4cdf4688060a385459ce8d3a4
Kotake <82296998+usagimai@users.noreply.github.com>
2022-02-23 14:50:42 +0800

    adjust the logic of sorting events

--- src/utils/sortUtils.js
+++ src/utils/sortUtils.js
@@ -2,19 +2,19 @@ import moment from "moment";
 
 export const sortEvent = (allEvents, filteredProjects, todos) => {
   //排序行事曆每日顯示工作，依當天日期與該工作的完成期限之距離判斷：
-  // - 完成期限早於等於今天者，對resource push進「1」(排序最前、背景紅色字白色)
-  // - 完成期限為明天者，對resource push進「3」(排序第二、背景橘色字白色)
-  // - 完成期限為後天以後者，對resource push進「5」(排序第三、背景正常字正常)
+  // - 完成期限早於等於執行當天者，對resource push進「1」(排序最前、背景紅色字白色)
+  // - 完成期限為執行當天的隔天者，對resource push進「3」(排序第二、背景橘色字白色)
+  // - 完成期限為執行當天的兩天後者，對resource push進「5」(排序第三、背景正常字正常)
   const addPriority = [];
   const addPriorityLogic = allEvents.map((event) => {
     const deadline = moment(event.resource[3]);
-    const today = moment(new Date()).format("YYYY/MM/DD");
+    const todoDate = moment(event.start).format("YYYY/MM/DD");
 
     switch (true) {
-      case deadline.diff(today, "days") <= 0:
+      case deadline.diff(todoDate, "days") <= 0:
         event.resource.push("1");
         break;
-      case deadline.diff(today, "days") === 1:
+      case deadline.diff(todoDate, "days") === 1:
         event.resource.push("3");
         break;
       default:
@@ -56,20 +56,17 @@ export const sortEvent = (allEvents, filteredProjects, todos) => {
     }
   });
 
-  //排序 (先以優先順序代號排，若相同，以今天離完成期限近→遠排)
+  //排序 (先以優先順序代號排，若相同，以執行當天離完成期限近→遠排)
   addPriority.sort((a, b) => {
     if (a.resource[4] !== b.resource[4]) {
       return a.resource[4] - b.resource[4];
     } else {
       return (
         moment(a.resource[3]).diff(
-          moment(new Date()).format("YYYY/MM/DD"),
+          moment(a.start).format("YYYY/MM/DD"),
           "days"
         ) -
-        moment(b.resource[3]).diff(
-          moment(new Date()).format("YYYY/MM/DD"),
-          "days"
-        )
+        moment(b.resource[3]).diff(moment(b.start).format("YYYY/MM/DD"), "days")
       );
     }
   });
